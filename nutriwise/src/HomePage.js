997982e5d21363fda6958bef4dcd331dfce733f6
Rohini Arangam <rohinia9@cs.washington.edu>
2023-05-13 15:36:42 -0700

    indents

--- nutriwise/src/HomePage.js
+++ nutriwise/src/HomePage.js
@@ -1,12 +1,13 @@
 import {React} from 'react'
 function HomePage(){
 return (
-    <div>
-<h1>HomePage</h1>
-<h1>NutriGenie</h1>
+<div>
+   <div>
+        <h1>HomePage</h1>
+        <h1>NutriGenie</h1>
       <h2>Prompt Generator</h2>
-
     </div>
+</div>
 )
 }
 
